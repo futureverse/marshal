6c7c2675d66f35915b4ab78256c82ed635271226
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-09-23 11:32:17 -0700

    Add marshal() for DBIConnection (not supported)

---
+++ R/marshal.DBIConnection.R
@@ -0,0 +1,18 @@
+#' Marshal and Unmarshal a 'DBI:DBIConnection' object (not supported)
+#'
+#' _Warning: Objects of this class are not possible to marshal.
+#'  If attempted, an error is produced._
+#'
+#' @param x A [DBI::DBIConnection-class] object.
+#'
+#' @param \dots Not used.
+#'
+#' @return
+#' A `marshalled` object as described in [marshal()].
+#'
+#' @rdname marshal.DBIConnection
+#' @aliases marshal.DBIConnection
+#' @export
+marshal.DBIConnection <- function(x, ...) {
+  stop(MarshalNotSupportedError(object = x))
+}
---
+++ tests/marshal.DBIConnection.R
@@ -0,0 +1,17 @@
+library(marshal)
+
+if (requireNamespace("DBI", quietly = TRUE) &&
+    requireNamespace("RSQLite", quietly = TRUE)) {
+  con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  print(con)
+  
+  ## Marshal read-only connection, which records the
+  ## current state, including the current file position.
+  res <- tryCatch({
+    marshal(con)
+  }, error = identity)
+  stopifnot(
+    inherits(res, "error"),
+    inherits(res, "MarshalNotSupportedError")
+  )
+}
