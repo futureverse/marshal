e321862e6631acf5f9c384abf869814c764990f5
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-10-06 13:22:21 -0700

    TESTS: rstan; make it run single-threaded - just in case it doesn't

--- tests/marshal.rstan.R
+++ tests/marshal.rstan.R
@@ -2,6 +2,7 @@ library(marshal)
 
 if (requireNamespace("BH", quietly = TRUE) && requireNamespace("RcppEigen", quietly = TRUE) && requireNamespace("rstan", quietly = TRUE)) {
   library(rstan)
+  rstan_options(threads_per_chain = 1L)  ## avoid using too many cores
 
   code <- "
   data {
